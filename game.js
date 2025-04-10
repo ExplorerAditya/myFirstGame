cba14658c8f59853a76b26a9feeb64152bf1bd46
Aditya Singh <adityasingh.privatemail@gmail.com>
2025-04-11 00:57:46 +0530

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-//game 
-let gameNum=25;
-let num=prompt("Guess the game number");
-while(num!=gameNum)
-{
-   num= prompt ("Wrong guess, try again");
-}
-alert("You guessed it right! The game number is " + gameNum);
-
